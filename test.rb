a4baf9585d2ba1d9a29e58b459b895b43b17f2bf
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2014-05-11 16:56:32 +0900

    Add testing to Entry class

--- test.rb
+++ test.rb
@@ -25,4 +25,34 @@ class BlogGemTest < MiniTest::Unit::TestCase
       comment.text
       )
   end
+
+  def test_escape_entry
+    entry = Entry.new
+    entry.body = "<p>hello\ngem\n</p><pre class='format'>hello\ngem\n</pre>aaa"
+    assert_equal(
+      "<p>hello<br />gem<br /></p><pre class='format'>hello\ngem\n</pre>aaa",
+      entry.text
+      )
+
+    entry = Entry.new
+    entry.body = "<pre>a\nb</pre><pre>a\nb</pre><pre>a\nb</pre>"
+    assert_equal(
+      "<pre>a\nb</pre><pre>a\nb</pre><pre>a\nb</pre>",
+      entry.text
+      )
+
+    entry = Entry.new
+    entry.body = "<pre>a\nb</pre>d[read_more]\ne<pre>a\nb</pre><pre>a\nb</pre>"
+    assert_equal(
+      "<pre>a\nb</pre>d",
+      entry.text
+      )
+
+    entry = Entry.new
+    entry.body = "<pre>a\nb</pre>d[read_more]\ne<pre>a\nb</pre><pre>a\nb</pre>"
+    assert_equal(
+      "<pre>a\nb</pre>d<br />e<pre>a\nb</pre><pre>a\nb</pre>",
+      entry.text(false)
+      )
+  end
 end
